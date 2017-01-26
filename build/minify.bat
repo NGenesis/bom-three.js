c4fb85bf1045f9ed9b5a4a0ae7153bbd04ff2a02
David Cormack <david@sentrika.net>
2017-01-26 06:54:23 +0000

    Fixed file paths for minify batch file.

--- build/minify.bat
+++ build/minify.bat
@@ -1,2 +1,2 @@
 @echo off
-uglifyjs2 examples/js/loaders/BOMLoader.js -m -o examples/js/loaders/BOMLoader.min.js -c
+uglifyjs2 ../examples/js/loaders/BOMLoader.js -m -o ../examples/js/loaders/BOMLoader.min.js -c
